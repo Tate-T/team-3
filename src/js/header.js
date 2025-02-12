9a2448e03fc4f2937a75323709dc38981154c500
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-02-12 20:17:38 +0200

    start js

---
+++ src/js/header.js
@@ -0,0 +1,4 @@
+const buttonElement = document.querySelector(".header__img")
+buttonElement.addEventListener("click", (e) => {
+    buttonElement.setAttribute("src", "../img/dark-switch.webp")
+})
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import "./js/modal.js"
+import "./js/header.js"
 import "./js/calculator.js"
 import "./js/year.js"
