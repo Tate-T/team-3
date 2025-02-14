63f5fad69824ee78ffc37b29beb537d04b8fab47
pro100mq <144606932+pro100mq@users.noreply.github.com>
2025-02-14 22:30:02 +0200

    fix bugs

--- src/js/our-team.js
+++ src/js/our-team.js
@@ -12,39 +12,40 @@ arrowLeft.addEventListener("click", () => {
     name1.innerHTML = "Кіріл Присакар";
     text.innerHTML =
       "Скром-майстер, виконував такі секції як: Hero, перевір в який рік народився, камінь-ножиці-папір, обидва калькулятори, наша команда та частково обери вченого";
-    element1.style.width = "30px";
+    element1.style.width = "60px";
     element1.style.backgroundColor = "#797979";
-    element2.style.width = "18px";
+    element2.style.width = "36px";
     element2.style.backgroundColor = "#D9D9D9";
   } else {
     image.src = "../img/photo1.webp";
     name1.innerHTML = "Кіріл Присакар";
     text.innerHTML =
       "Скром-майстер, виконував такі секції як: Hero, перевір в який рік народився, камінь-ножиці-папір, обидва калькулятори, наша команда та частково обери вченого";
-    element1.style.width = "30px";
+    element1.style.width = "60px";
     element1.style.backgroundColor = "#797979";
-    element2.style.width = "18px";
+    element2.style.width = "36px";
     element2.style.backgroundColor = "#D9D9D9";
   }
 });
 
 arrowRight.addEventListener("click", () => {
   if (image.src.endsWith("photo1.webp")) {
+    image.src = "../img/photo2.webp";
     name1.innerHTML = "Іван Довгаль";
     text.innerHTML =
       "Робив такі секції як: header, вгадай число, google динозаврика, футбол, footer введіть 3 числа та частково обери вченого";
-    element2.style.width = "30px";
+    element2.style.width = "60px";
     element2.style.backgroundColor = "#797979";
-    element1.style.width = "18px";
+    element1.style.width = "36px";
     element1.style.backgroundColor = "#D9D9D9";
   } else {
-    image.src = "../img/photo1.webp";
-    name1.innerHTML = "Кіріл Присакар";
+    image.src = "../img/photo2.webp";
+    name1.innerHTML = "Іван Довгаль";
     text.innerHTML =
-      "Скром-майстер, виконував такі секції як: Hero, перевір в який рік народився, камінь-ножиці-папір, обидва калькулятори, наша команда та частково обери вченого";
-    element1.style.width = "30px";
-    element1.style.backgroundColor = "#797979";
-    element2.style.width = "18px";
-    element2.style.backgroundColor = "#D9D9D9";
+      "Робив такі секції як: header, вгадай число, google динозаврика, футбол, footer введіть 3 числа та частково обери вченого";
+    element2.style.width = "60px";
+    element2.style.backgroundColor = "#797979";
+    element1.style.width = "36px";
+    element1.style.backgroundColor = "#D9D9D9";
   }
 });
