c853d0a0dafadc55f27a9155d04db3c20d02052c
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-02-18 21:43:27 +0200

    end-work

--- src/js/header.js
+++ src/js/header.js
@@ -1,29 +1,31 @@
 const imgElement = document.querySelector(".header__img");
 imgElement.addEventListener("click", (e) => {
     const imgSrc = imgElement.src;
+    const allElements = document.querySelectorAll("*");
+    const inputElements = document.querySelectorAll("input");
+
     if (imgSrc.includes("light-switch.webp")) {
         imgElement.src = "../img/dark-switch.webp";
-        document.body.style.backgroundColor = "#120214"
-        const allElements = document.querySelectorAll("*");
+        document.body.style.backgroundColor = "#3a3a3a";
+
         allElements.forEach((el) => { 
             el.style.color = "white"; 
         });
-        const inputElements = document.querySelectorAll("input")
+
         inputElements.forEach((el) => {
             el.style.color = "black"; 
-        })
+        });
 
-        
-        
     } else {
         imgElement.src = "../img/light-switch.webp";
-        document.body.style.backgroundColor = "white"
+        document.body.style.backgroundColor = "white";
+
         allElements.forEach((el) => { 
             el.style.color = "black"; 
         });
-    }
+
         inputElements.forEach((el) => {
             el.style.color = "white"; 
-        })
+        });
+    }
 });
-
