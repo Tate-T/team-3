64647810a41961cfa9fafa4f8d75af4ac556a1c2
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-02-23 14:16:10 +0200

    see

--- src/js/our-team.js
+++ src/js/our-team.js
@@ -8,7 +8,7 @@ const element2 = document.querySelector("#second-element");
 
 arrowLeft.addEventListener("click", () => {
   if (image.src.endsWith("photo1.webp")) {
-    image.src = "./img/photo1.webp";
+    image.src = "../img/photo1.webp";
     name1.innerHTML = "Кіріл Присакар";
     text.innerHTML =
       "Скром-майстер, виконував такі секції як: Hero, перевір в який рік народився, камінь-ножиці-папір, обидва калькулятори, наша команда та частково обери вченого";
@@ -17,7 +17,7 @@ arrowLeft.addEventListener("click", () => {
     element2.style.width = "36px";
     element2.style.backgroundColor = "#D9D9D9";
   } else {
-    image.src = "./img/photo1.webp";
+    image.src = "../img/photo1.webp";
     name1.innerHTML = "Кіріл Присакар";
     text.innerHTML =
       "Скром-майстер, виконував такі секції як: Hero, перевір в який рік народився, камінь-ножиці-папір, обидва калькулятори, наша команда та частково обери вченого";
@@ -30,7 +30,7 @@ arrowLeft.addEventListener("click", () => {
 
 arrowRight.addEventListener("click", () => {
   if (image.src.endsWith("photo1.webp")) {
-    image.src = "./img/photo2.webp";
+    image.src = "../img/photo2.webp";
     name1.innerHTML = "Іван Довгаль";
     text.innerHTML =
       "Робив такі секції як: header, вгадай число, google динозаврика, футбол, footer введіть 3 числа та частково обери вченого";
@@ -39,7 +39,7 @@ arrowRight.addEventListener("click", () => {
     element1.style.width = "36px";
     element1.style.backgroundColor = "#D9D9D9";
   } else {
-    image.src = "./img/photo2.webp";
+    image.src = "../img/photo2.webp";
     name1.innerHTML = "Іван Довгаль";
     text.innerHTML =
       "Робив такі секції як: header, вгадай число, google динозаврика, футбол, footer введіть 3 числа та частково обери вченого";
