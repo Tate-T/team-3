a60906f960fc5d06811f7b783385207e90f50afa
pro100mq <144606932+pro100mq@users.noreply.github.com>
2025-02-28 23:30:45 +0200

    fix

--- src/js/our-team.js
+++ src/js/our-team.js
@@ -7,7 +7,7 @@ const element1 = document.querySelector("#first-element");
 const element2 = document.querySelector("#second-element");
 
 arrowLeft.addEventListener("click", () => {
-  if (image.src.endsWith("photo1.webp")) {
+  if (image.src.endsWith("photo2.webp")) {
     image.src = "./img/photo1.webp";
     name1.innerHTML = "Кіріл Присакар";
     text.innerHTML =
@@ -18,13 +18,13 @@ arrowLeft.addEventListener("click", () => {
     element2.style.backgroundColor = "#D9D9D9";
   } else if (image.src.endsWith("photo2.webp")){
     image.src = "./img/photo1.webp";
-    name1.innerHTML = "Кіріл Присакар";
+    name1.innerHTML = "Іван Довгаль";
     text.innerHTML =
-      "Скром-майстер, виконував такі секції як: Hero, перевір в який рік народився, камінь-ножиці-папір, обидва калькулятори, наша команда та частково обери вченого";
-    element1.style.width = "60px";
-    element1.style.backgroundColor = "#797979";
-    element2.style.width = "36px";
-    element2.style.backgroundColor = "#D9D9D9";
+      "Робив такі секції як: header, вгадай число, google динозаврика, футбол, footer введіть 3 числа та частково обери вченого";
+    element2.style.width = "60px";
+    element2.style.backgroundColor = "#797979";
+    element1.style.width = "36px";
+    element1.style.backgroundColor = "#D9D9D9";
   }
 });
 
