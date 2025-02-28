a7aa3d2abdbd0d1ec8b3a56ae330523580b309a8
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2025-02-28 22:18:52 +0200

    fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,22 +1,28 @@
-const imgElement = document.querySelector(".header__img");
-
-imgElement.addEventListener("click", () => {
-    const imgSrc = imgElement.src;
-    const allElements = document.querySelectorAll("body *:not(button)");
-    const inputElements = document.querySelectorAll("input");
-
-    if (imgSrc.includes("light-switch.webp")) {
-        imgElement.src = "../img/dark-switch.webp";
-        document.body.style.backgroundColor = "#3a3a3a";
-
-        allElements.forEach((el) => el.style.color = "white");
-        inputElements.forEach((el) => el.style.color = "black");
-
-    } else {
-        imgElement.src = "../img/light-switch.webp";
-        document.body.style.backgroundColor = "white";
-
-        allElements.forEach((el) => el.style.color = "black");
-        inputElements.forEach((el) => el.style.color = "white");
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    const imgLight = document.querySelector(".header__img1");
+    const imgDark = document.querySelector(".header__img2");
+    
+    imgDark.style.display = "none";
+    
+    const toggleTheme = () => {
+        const allElements = document.querySelectorAll("body *:not(button)");
+        const inputElements = document.querySelectorAll("input");
+        
+        if (imgLight.style.display !== "none") {
+            imgLight.style.display = "none";
+            imgDark.style.display = "block";
+            document.body.style.backgroundColor = "#3a3a3a";
+            allElements.forEach((el) => el.style.color = "white");
+            inputElements.forEach((el) => el.style.color = "black");
+        } else {
+            imgLight.style.display = "block";
+            imgDark.style.display = "none";
+            document.body.style.backgroundColor = "white";
+            allElements.forEach((el) => el.style.color = "black");
+            inputElements.forEach((el) => el.style.color = "white");
+        }
+    };
+    
+    imgLight.addEventListener("click", toggleTheme);
+    imgDark.addEventListener("click", toggleTheme);
 });
